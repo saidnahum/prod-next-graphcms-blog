047d6fd07f64a05933b1c6541755fcecfb129065
saidnahum <saidnahum@gmail.com>
2021-11-23 15:00:27 -0600

    finished app

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,15 @@
-import { useContext } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import Link from 'next/link';
-
-const categories = [
-   {name: 'React', slug:'react'}, 
-   {name: 'Web Development', slug:'web-dev'}
-]
+import { getCategories } from '../services';
 
 const Header = () => {
+
+   const [categories, setCategories] = useState([]);
+   useEffect(() => {
+      getCategories()
+         .then((newCategories) => setCategories(newCategories));
+   }, [])
+
    return (
       <div className='container mx-auto px-10 mb-8'>
          <div className='border-b w-full inline-block border-blue-400 py-8'>
---
+++ pages/api/comments.js
@@ -0,0 +1,28 @@
+import { GraphQLClient, gql } from 'graphql-request';
+
+const graphqlAPI = process.env.NEXT_PUBLIC_PIXELBLOG_ENDPOINT;
+const graphcmsToken = process.env.GRAPHCMS_TOKEN;
+
+export default async function comments(req, res){
+
+  const { name, email, slug, comment } = req.body; 
+
+  const graphQLClient = new GraphQLClient(graphqlAPI, {
+    headers: {
+      authorization: `Bearer ${graphcmsToken}`
+    }
+  });
+
+  const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
+      createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}){id}
+    }
+  `
+  try {
+    const result = await graphQLClient.request(query, req.body)
+
+    return res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
